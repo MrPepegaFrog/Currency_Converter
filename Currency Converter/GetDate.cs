6f8edd117c6f91704c61d1f1d1da772911e5d64e
MrPepegaFrog <truepepega@internet.ru>
2023-09-20 01:30:13 +0500

    create date timer and basic UI

---
+++ Currency Converter/GetDate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Currency_Converter
+{
+    public class GetDate 
+    {
+      /*  
+        public void onGetdate()
+        {
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(dispatcherTimer_Tick);
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Start();
+
+           
+
+
+        }
+
+        public void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            DateTime dateTime = DateTime.Now;
+            
+        }
+
+        */
+    }
+}
--- Currency Converter/MainWindow.xaml.cs
+++ Currency Converter/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+
+
 
 namespace Currency_Converter
 {
@@ -20,9 +24,41 @@ namespace Currency_Converter
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+            GetDate();
+           
         }
+
+        
+
+
+
+
+
+         private void GetDate()
+         {
+
+             DispatcherTimer timer = new DispatcherTimer();
+             timer.Tick += new EventHandler(dispatcherTimer_Tick);
+             timer.Interval = TimeSpan.FromSeconds(1);
+             timer.Start();
+
+
+
+
+
+         }
+
+
+         private void dispatcherTimer_Tick(object sender, EventArgs e)
+         {
+             DateTime dateTime = DateTime.Now;
+             dateField.Text = dateTime.ToString();
+         }
+
+         
     }
 }
